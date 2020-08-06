18d3867cbe2aabd63930888bb946082b5f8c067e
Ashwin Tyagi <ashwin.k.tyagi@gmail.com>
2020-08-06 14:27:36 -0700

    commit

--- PlannED/Resources/CollegeViewController.swift
+++ PlannED/Resources/CollegeViewController.swift
@@ -15,28 +15,13 @@ class CollegeViewController: UIViewController{
     
     @IBOutlet weak var instName: UILabel!
     
-    @IBOutlet weak var satvr25: UILabel!
-    @IBOutlet weak var satvr75: UILabel!
-    @IBOutlet weak var satmt25: UILabel!
-    @IBOutlet weak var satmt75: UILabel!
-    @IBOutlet weak var satwr25: UILabel!
-    @IBOutlet weak var satwr75: UILabel!
-    @IBOutlet weak var satvrmid: UILabel!
-    @IBOutlet weak var satmtmid: UILabel!
-    @IBOutlet weak var satwrmid: UILabel!
+    @IBOutlet weak var satTotal25: UILabel!
+    @IBOutlet weak var satTotal75: UILabel!
+    @IBOutlet weak var satTotalMid: UILabel!
     
-    @IBOutlet weak var actcm25: UILabel!
-    @IBOutlet weak var actcm75: UILabel!
-    @IBOutlet weak var acten25: UILabel!
-    @IBOutlet weak var acten75: UILabel!
-    @IBOutlet weak var actmt25: UILabel!
-    @IBOutlet weak var actmt75: UILabel!
-    @IBOutlet weak var actwr25: UILabel!
-    @IBOutlet weak var actwr75: UILabel!
-    @IBOutlet weak var actcmmid: UILabel!
-    @IBOutlet weak var actenmid: UILabel!
-    @IBOutlet weak var actmtmid: UILabel!
-    @IBOutlet weak var actwrmid: UILabel!
+    @IBOutlet weak var actTotal25: UILabel!
+    @IBOutlet weak var actTotal75: UILabel!
+    @IBOutlet weak var actTotalMid: UILabel!
     
     let ref = Database.database().reference()
     
@@ -46,35 +31,30 @@ class CollegeViewController: UIViewController{
         
         ref.child("colleges").child(Helper.tempCollegeID).observeSingleEvent(of: .value, with: { (snapshot) in
             
+            let formatter = NumberFormatter()
             let value = snapshot.value as? NSDictionary
+            
             
             self.instName.text = value?["INSTNM"] as? String ?? ""
             
-            self.satvr25.text = value?["SATVR25"] as? String ?? ""
-            self.satvr75.text = value?["SATVR75"] as? String ?? ""
-            self.satmt25.text = value?["SATMT25"] as? String ?? ""
-            self.satmt75.text = value?["SATMT75"] as? String ?? ""
-            self.satwr25.text = value?["SATWR25"] as? String ?? ""
-            self.satwr75.text = value?["SATWR75"] as? String ?? ""
-            self.satvrmid.text = value?["SATVRMID"] as? String ?? ""
-            self.satmtmid.text = value?["SATMTMID"] as? String ?? ""
-            self.satwrmid.text = value?["SATWRMID"] as? String ?? ""
+            var x = formatter.number(from: value?["SATVR25"] as? String ?? "") as! Int
+            var y = formatter.number(from: value?["SATMT25"] as? String ?? "") as! Int
+            var total = x + y
+            self.satTotal25.text = formatter.string(from: NSNumber(value: total))
             
-            self.actcm25.text = value?["ACTCM25"] as? String ?? ""
-            self.actcm75.text = value?["ACTCM75"] as? String ?? ""
-            self.acten25.text = value?["ACTEN25"] as? String ?? ""
-            self.acten75.text = value?["ACTEN75"] as? String ?? ""
-            self.actmt25.text = value?["ACTMT25"] as? String ?? ""
-            self.actmt75.text = value?["ACTMT75"] as? String ?? ""
-            self.actwr25.text = value?["ACTWR25"] as? String ?? ""
-            self.actwr75.text = value?["ACTWR75"] as? String ?? ""
-            self.actcmmid.text = value?["ACTCMMID"] as? String ?? ""
-            self.actenmid.text = value?["ACTENMID"] as? String ?? ""
-            self.actmtmid.text = value?["ACTMTMID"] as? String ?? ""
-            self.actwrmid.text = value?["ACTWRMID"] as? String ?? ""
+            x = formatter.number(from: value?["SATVR75"] as? String ?? "") as! Int
+            y = formatter.number(from: value?["SATMT75"] as? String ?? "") as! Int
+            total = x + y
+            self.satTotal75.text = formatter.string(from: NSNumber(value: total))
             
+            x = formatter.number(from: value?["SATVRMID"] as? String ?? "") as! Int
+            y = formatter.number(from: value?["SATMTMID"] as? String ?? "") as! Int
+            total = x + y
+            self.satTotalMid.text = formatter.string(from: NSNumber(value: total))
             
-            
+            self.actTotal25.text = value?["ACTCM25"] as? String ?? ""
+            self.actTotal75.text = value?["ACTCM75"] as? String ?? ""
+            self.actTotalMid.text = value?["ACTCMMID"] as? String ?? ""
             
         })
     }
